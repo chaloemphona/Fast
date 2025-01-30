8883aa31deab528b12f391dab2bb7f55796e3756
Chaloemphon <150015147+chaloemphona@users.noreply.github.com>
2025-01-30 19:31:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,12 @@ security = HTTPBearer()
 def read_root():
     return {"message": "Hello World"}
 
+@app.get("/api/v1", tags=["Docs"])
+async def message():
+    return { "H e l l o  W o r l d "}
+
+
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))  # Use Render's PORT
     uvicorn.run(app, host="0.0.0.0", port=port)
-
-
-
-@app.get("/api/v1", tags=["Docs"])
-async def message():
-    """
-    Test that the domain is valid and the endpoint is working.
-    """
-    return { "H e l l o  W o r l d "}

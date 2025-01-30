ff640f45f95001ce643032d467fa7f3510552a4d
Chaloemphon <150015147+chaloemphona@users.noreply.github.com>
2025-01-30 19:33:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import os
 from fastapi import FastAPI
 
-
 app = FastAPI()
-security = HTTPBearer()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
 
-@app.get("/api/v1", tags=["Docs"])
+@app.get("/api/v1")
 async def message():
     return { "H e l l o  W o r l d "}
 
@@ -18,3 +16,5 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))  # Use Render's PORT
     uvicorn.run(app, host="0.0.0.0", port=port)
+
+
